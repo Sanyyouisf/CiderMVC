dd315c707e527fdf5fbed1231336f74cbb4ea697
Nathan Gonzalez <nathan@copypastedeveloper.com>
2017-10-10 18:38:03 -0500

    oops

---
+++ CiderAndCode.Web/ViewModels/MakeCiderRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CiderAndCode.Web.ViewModels
+{
+    public class MakeCiderRequest
+    {
+        [Required]
+        [Display(Name = "Select Bushel")]
+        public int BushelId { get; set; }
+    }
+}
